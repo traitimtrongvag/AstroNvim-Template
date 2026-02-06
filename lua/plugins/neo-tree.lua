4f59441e7e7003663fe203ae1b6721efc661ac85
traitimtrongvag <pan2512811@gmail.com>
2026-02-06 23:33:24 +0700

    feat: add neo-tree config to show hidden files by default

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+---@type LazySpec
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+        never_show = {
+          ".DS_Store",
+          "thumbs.db",
+        },
+      },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+    },
+  },
+}
